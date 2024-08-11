e8198f0a20b177fe5a8e5ef135b66c839facf27b
adibkn1 <adibkn1@gmail.com>
2024-08-12 05:28:08 +0530

    ..

--- docs/packed.js
+++ docs/packed.js
@@ -56551,9 +56551,13 @@ async function startCameraKit() {
       apiToken: 'eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNzA2NzExNzk4LCJzdWIiOiJhNWQ0ZjU2NC0yZTM0LTQyN2EtODI1Ni03OGE2NTFhODc0ZTR-U1RBR0lOR35mMzBjN2JmNy1lNjhjLTRhNzUtOWFlNC05NmJjOTNkOGIyOGYifQ.xLriKo1jpzUBAc1wfGpLVeQ44Ewqncblby-wYE1vRu0'
     });
 
+    // Obtain and configure the media stream for high resolution
+    let mediaStream = await navigator.mediaDevices.getUserMedia({
+      video: { width: 4096, height: 2160, facingMode: 'environment' }
+
+    });
     // Create a new session and optionally handle higher resolution settings
     const session = await cameraKit.createSession({
-      cameraResolution: '1080p'  // This is indicative; actual API parameters may vary
     });
 
     const canvasElement = document.getElementById('canvas');
@@ -56565,11 +56569,7 @@ async function startCameraKit() {
       const { lenses } = await cameraKit.lensRepository.loadLensGroups(['fdd0879f-c570-490e-9dfc-cba0f122699f']);
       session.applyLens(lenses[0]);
 
-      // Obtain and configure the media stream for high resolution
-      let mediaStream = await navigator.mediaDevices.getUserMedia({
-        video: { width: 4096, height: 2160, facingMode: 'environment' }
-
-      });
+      
 
       const source = createMediaStreamSource(mediaStream, { cameraType: 'back' });
       await session.setSource(source);
