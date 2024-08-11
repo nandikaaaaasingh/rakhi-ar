51a8e448160d1ceb522ff862351c3a5ba6d7da73
adibkn1 <adibkn1@gmail.com>
2024-08-12 05:10:30 +0530

    k

--- docs/packed.js
+++ docs/packed.js
@@ -35736,7 +35736,7 @@ async function initCameraKit() {
 
     // Configure media stream with high resolution
     let mediaStream = await navigator.mediaDevices.getUserMedia({
-      video: { width: window.innerWidth, height: window.innerHeight, facingMode: 'environment' }
+      video: { width: 4096, height: 2160, facingMode: 'environment' }
     });
 
     const source = createMediaStreamSource(mediaStream, { cameraType: 'back' });
