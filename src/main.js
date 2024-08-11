44bc1db9c0b82d40bd3b5d22c7f84f718294c5cd
adibkn1 <adibkn1@gmail.com>
2024-08-12 04:05:36 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -23,7 +23,7 @@ async function startCameraKit() {
       session.applyLens(lenses[0]);
 
       let mediaStream = await navigator.mediaDevices.getUserMedia({
-        video: { width: window.innerWidth, height: window.innerHeight, facingMode: 'environment' }
+        video: { width: 4096, height: 2160, facingMode: 'environment' }
       });
 
       const source = createMediaStreamSource(mediaStream, { cameraType: 'back' });
