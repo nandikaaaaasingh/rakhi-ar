daf092e177e7e3ecf8476ca83ff1b516c2e8b75e
adibkn1 <adibkn1@gmail.com>
2024-08-12 05:22:42 +0530

    a

--- src/main.js
+++ src/main.js
@@ -5,28 +5,24 @@ import {
   createMediaStreamSource,
 } from '@snap/camera-kit';
 
-// Function to initialize the camera kit session
 async function initCameraKit() {
   try {
-    // Initialize the camera kit with the appropriate API token
     const cameraKit = await bootstrapCameraKit({
       apiToken: 'eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNzA2NzExNzk4LCJzdWIiOiJhNWQ0ZjU2NC0yZTM0LTQyN2EtODI1Ni03OGE2NTFhODc0ZTR-U1RBR0lOR35mMzBjN2JmNy1lNjhjLTRhNzUtOWFlNC05NmJjOTNkOGIyOGYifQ.xLriKo1jpzUBAc1wfGpLVeQ44Ewqncblby-wYE1vRu0'
     });
-
-    // Create a new session
-    const session = await cameraKit.createSession();
-
-    // Append the live output to the body
-    document.body.appendChild(session.output.live);
-
-    // Load and apply lenses
-    const { lenses } = await cameraKit.lensRepository.loadLensGroups(['fdd0879f-c570-490e-9dfc-cba0f122699f']);
-      session.applyLens(lenses[0]);
 
     // Configure media stream with high resolution
     let mediaStream = await navigator.mediaDevices.getUserMedia({
       video: { width: 4096, height: 2160, facingMode: 'environment' }
     });
+
+    // Create a new session
+    const session = await cameraKit.createSession();
+    document.body.appendChild(session.output.live);
+    const { lenses } = await cameraKit.lensRepository.loadLensGroups(['fdd0879f-c570-490e-9dfc-cba0f122699f']);
+      session.applyLens(lenses[0]);
+
+  
 
     const source = createMediaStreamSource(mediaStream, { cameraType: 'back' });
     await session.setSource(source);
