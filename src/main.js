ea1b2b8e192f43f541bf3030810d92a55c8a59b0
adibkn1 <adibkn1@gmail.com>
2024-08-14 05:04:13 +0530

    sdf

--- src/main.js
+++ src/main.js
@@ -218,8 +218,8 @@ function drawVideoToCanvas(videoElement, canvas) {
   const context = canvas.getContext('2d');
 
   // Set the canvas dimensions to match the video
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
+  canvas.width = window.innerWidth*window.devicePixelRatio;
+  canvas.height = window.innerHeight*window.devicePixelRatio;
 
   function drawFrame() {
     // Clear the canvas before drawing
