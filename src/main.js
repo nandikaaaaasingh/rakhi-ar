bc53c3642faa0c626f01cfffa9cc1aeb8cdd4978
adibkn1 <adibkn1@gmail.com>
2024-08-14 15:08:44 +0530

    d

--- src/main.js
+++ src/main.js
@@ -106,8 +106,8 @@ function showReceiverSide(token) {
       const rakhiData = Object.values(data).find((entry) => entry.token === token);
       if (rakhiData) {
         document.getElementById('greeting').innerHTML = `
-          <span class="greeting-title">HEY!</span><br>
-          <span class="greeting-message">${rakhiData.brotherName}, your sister has sent you a special digital rakhi to celebrate the bond you share.</span>
+          <span class="greeting-title">HEY! ${rakhiData.brotherName}, </span><br>
+          <span class="greeting-message"> your sibling has sent you a special digital rakhi to celebrate the bond you share.</span>
         `;
 
         document.getElementById('greeting-overlay').innerHTML = `
