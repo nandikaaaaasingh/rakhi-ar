a8133d7d41d523b24af4afbbabbd8faf89aa97d6
adibkn1 <adibkn1@gmail.com>
2024-08-16 13:25:04 +0530

    ,

--- src/main.js
+++ src/main.js
@@ -194,10 +194,9 @@ async function handleSharing(link, brotherName) {
     alert('Failed to share or copy link. Please try manually.');
   } finally {
     // Redirect to the thank you page regardless of the share outcome
-    window.location.href = `thank-you.html`;
+    window.location.href = thank-you.html;
   }
 }
-
 
 async function startCameraKit(rakhiData) {
   const cameraContainer = document.getElementById('camera-container');
