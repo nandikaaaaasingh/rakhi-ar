8ec653746de4818537f14e10163cfe1a7d3552e2
adibkn1 <adibkn1@gmail.com>
2024-08-16 11:02:16 +0530

    vecdwx

--- src/main.js
+++ src/main.js
@@ -116,13 +116,13 @@ function showReceiverSide(token) {
       if (rakhiData) {
         document.getElementById('greeting').innerHTML = `
           <span class="greeting-title">HEY </span><br>
-          <span class="greeting-title">${rakhiData.brotherName}!,</span><br>
+          <span class="greeting-title">${rakhiData.brotherName}!</span><br>
           <span class="greeting-message"> your sibling has sent you a special digital rakhi to celebrate the bond you share.</span>
         `;
 
         document.getElementById('greeting-overlay').innerHTML = `
           <span class="greeting-title">HEY</span><br>
-          <span class="greeting-message">${rakhiData.brotherName}!, your sibling has sent you a special digital rakhi to celebrate the bond you share.</span>
+          <span class="greeting-message">${rakhiData.brotherName}! your sibling has sent you a special digital rakhi to celebrate the bond you share.</span>
         `;
 
         receiverContainer.addEventListener('click', () => handleTap(receiverContainer, cameraContainer, rakhiData));
