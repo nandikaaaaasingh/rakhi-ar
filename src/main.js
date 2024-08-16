05dc585310915c77fd64082d322aed7764f5de81
adibkn1 <adibkn1@gmail.com>
2024-08-16 13:59:35 +0530

    dqs

--- src/main.js
+++ src/main.js
@@ -170,7 +170,7 @@ async function handleSharing(link) {
     if (navigator.share) {
       await navigator.share({
         title: 'Send Digital Rakhi',
-        text: 'Check out this digital Rakhi I sent you!',
+        text: `Dear ${rakhiData.brotherName},\n\nI'm sending you this digital Rakhi as a symbol of my love. Let’s celebrate the bond that unites us. A promise of lifelong bond – an initiative by Chandak Group.`,
         url: link
       });
       console.log('Thanks for sharing!');
